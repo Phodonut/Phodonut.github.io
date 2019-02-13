8ffad6da1f2bdc87151cba460429df79e1a23629
Richard Harker <rharker7@gmail.com>
2019-02-12 19:37:01 -0800

    Fixed stooopid mistake for test

--- scripts/main.js
+++ scripts/main.js
@@ -307,12 +307,11 @@ function newRow(amount){
 		image[i].setAttribute("class", "RowIMG"); //CURRENTLY THROWING ERROR File Not Found path
 		console.log("info 1: " + dimensions[csvPortSplit1.length-1][0].slice(0,-2));  //console logging test if first # of image source isn't beyond what array has been set up for?
 		console.log("Must be less than: " + (i+columns*imgRowCounter));
-		if((i+columns*imgRowCounter) < parseInt(dimensions[csvPortSplit1.length-1][0].slice(0,-2))){ //If image would be beyond what we have in array
+		if((i+columns*imgRowCounter) > parseInt(dimensions[csvPortSplit1.length-1][0].slice(0,-2))){ //If image would be beyond what we have in array
 			image[i].src = docLoc + "Images/imgArray1_1.jpg"; //FILE NOT FOUND ERROR AVOIDANCE, shouldn't happen anymore but just in case
-			console.log("success?");
+			console.log("Failed");
 		}else{
 			image[i].src = docLoc + "Images/" + imgArray[(i+columns*imgRowCounter)] + ".jpg";  //images/imgArray#.jpg
-			console.log("error 404 image not found branch");
 		}
 		indiv[i].appendChild(image[i]);
 		vignette[i] = document.createElement('div');
